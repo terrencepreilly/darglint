996f650876d5b4bd4f23814e844738c073f73c67
treilly <treilly@savantgroup.com>
2017-09-25 20:06:18 -0400

    Update driver to perform check.

--- darglint/driver.py
+++ darglint/driver.py
@@ -1,5 +1,35 @@
+"""Defines the command line interface for darglint."""
+import sys
+
+from redbaron import RedBaron
+
+from .darglint import (
+    get_functions_and_docstrings,
+    read_program,
+)
+from .lex import lex
+from .parse import parse_arguments
+
+
 def main():
-    print('The package lives!')
+    """Check the parameters of all functions and methods."""
+    program = read_program(sys.argv[1])
+    red = RedBaron(program)
+    functions = get_functions_and_docstrings(red.fst())
+    for name, argnames, has_return, docstring in functions:
+        docstring_arguments = set(parse_arguments(lex(docstring)))
+        actual_arguments = set(argnames) - {'self'}
+        if len(docstring_arguments) != len(actual_arguments):
+            print('In function {},'.format(name))
+            missing_in_doc = actual_arguments - docstring_arguments
+            missing_in_fun = docstring_arguments - actual_arguments
+            if len(missing_in_doc) > 0:
+                print(' - Docstring is missing arguments: {}'.format(
+                    missing_in_doc))
+            if len(missing_in_fun) > 0:
+                print(' + Docstring defines non-extant arguments: {}'.format(
+                    missing_in_fun))
+
 
 if __name__ == '__main__':
     main()
--- darglint/example.py
+++ darglint/example.py
@@ -1,11 +1,14 @@
 """An example of a program to parse."""
 
-def add_two_numbers(a, b):
+
+def add_two_numbers(a):
     """Add two numbers together
 
     Args:
-        a: The first number
-        b: The second number
+        a: The first number.
+        b: The second number.
+
     Returns: The sum of a and b.
+
     """
-    return a + b
+    return a
