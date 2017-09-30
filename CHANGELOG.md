a1d08b2013434ea1e74b55eac3ba4df28fb3aa31
treilly <treilly@savantgroup.com>
2017-09-29 20:15:37 -0400

    Minimalize formatting for error message.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,10 +7,20 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [0.0.2]
 
 ### Added
+
 - Changelog.
+
 - Handling for methods.  Checks for whether the method is a static
-method or a class method.  Doesn't prompt for documentation for
-"self", or "cls" when not appropriate.
+  method or a class method.  Doesn't prompt for documentation for
+  "self", or "cls" when not appropriate.
+
+- Change format for error messages to be less verbose.
+  The format is now:
+    ```
+    <function/method name>
+      - <missing argument>
+      + <extraneous argument>
+    ```
 
 ## [0.0.1] - 2017-09-18
 
--- darglint/driver.py
+++ darglint/driver.py
@@ -1,5 +1,6 @@
 """Defines the command line interface for darglint."""
 import sys
+from typing import List
 
 from redbaron import RedBaron
 
@@ -11,6 +12,22 @@ from .lex import lex
 from .parse import parse_arguments
 
 
+def _print_error_message(
+        procedure_name: str,
+        docstring_arguments: List[str],
+        actual_arguments: List[str]) -> None:
+    print('{}'.format(procedure_name))
+    missing_in_doc = actual_arguments - docstring_arguments
+    missing_in_fun = docstring_arguments - actual_arguments
+    if len(missing_in_doc) > 0:
+        for missing in missing_in_doc:
+            print('  - {}'.format(missing))
+    if len(missing_in_fun) > 0:
+        for missing in missing_in_fun:
+            print('  + {}'.format(missing))
+    print()
+
+
 def main():
     """Check the parameters of all functions and methods."""
     program = read_program(sys.argv[1])
@@ -20,15 +37,11 @@ def main():
         docstring_arguments = set(parse_arguments(lex(docstring)))
         actual_arguments = set(argnames) - {'self'}
         if len(docstring_arguments) != len(actual_arguments):
-            print('In function {},'.format(name))
-            missing_in_doc = actual_arguments - docstring_arguments
-            missing_in_fun = docstring_arguments - actual_arguments
-            if len(missing_in_doc) > 0:
-                print(' - Docstring is missing arguments: {}'.format(
-                    missing_in_doc))
-            if len(missing_in_fun) > 0:
-                print(' + Docstring defines non-extant arguments: {}'.format(
-                    missing_in_fun))
+            _print_error_message(
+                name,
+                docstring_arguments,
+                actual_arguments,
+            )
 
 
 if __name__ == '__main__':
