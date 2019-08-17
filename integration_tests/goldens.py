c0ae616855c6e0083ad6a33fd5f3d8007d32bdf2
terrencepreilly <terrencepreilly@gmail.com>
2019-08-16 23:42:15 -0400

    Add basic goldens test.
    This integration test compares the actual sections captured by the
    parser to an ideal "golden" standard.
    
    Also, fixes optional newlines not being caught in sphinx docstrings.
    (Caught by goldens!)
    
    The goldens themselves are expected to live in "goldens.json".  Since
    these goldens could come from many different codebases, I'm not going
    to include them for the time being (in case there are license issues.)
    To create goldens, you can use the tools in the `doc_extract/`
    directory.  To do so, you first run the doc_extract tool on a list of
    github repositories:
    
        doc_extract -s <github-repos> -o <output-json-file>
    
    You can then annotate the docstrings by compiling the annotation tool
    and serving from that directory:
    
        cd static
        make
        python3 -m http.server
    
    You can upload the JSON file, add your annotations, then download the
    annotated version.  This can then be used in the goldens integration
    test.

---
+++ integration_tests/goldens.py
@@ -0,0 +1,97 @@
+"""Tests against goldens contained in a custom JSON file.
+
+Goldens can be created using the doc_extract utility in the bin/ folder.
+Goldens may not specify all parts of a docstring, but in order for
+the integration test to pass, all parts identified in the golden must
+be identified using the current implementation.
+
+"""
+
+import json
+import sys
+from unittest import (
+    TestCase,
+)
+from darglint.docstring.docstring import (
+    Docstring,
+)
+from darglint.docstring.base import (
+    Sections,
+)
+
+# class Sections(enum.Enum):
+#     SHORT_DESCRIPTION = 0
+#     LONG_DESCRIPTION = 1
+#     ARGUMENTS_SECTION = 2
+#     RAISES_SECTION = 4
+#     YIELDS_SECTION = 6
+#     RETURNS_SECTION = 8
+#     VARIABLES_SECTION = 10
+#     NOQAS = 13
+
+SECTION_MAP = {
+    'args': Sections.ARGUMENTS_SECTION,
+    'arguments': Sections.ARGUMENTS_SECTION,
+    'arguments-section': Sections.ARGUMENTS_SECTION,
+    'except': Sections.RAISES_SECTION,
+    'long-description': Sections.LONG_DESCRIPTION,
+    'params': Sections.ARGUMENTS_SECTION,
+    'raise': Sections.RAISES_SECTION,
+    'raises': Sections.RAISES_SECTION,
+    'raises-section': Sections.RAISES_SECTION,
+    'return': Sections.RETURNS_SECTION,
+    'returns': Sections.RETURNS_SECTION,
+    'returns-section': Sections.RETURNS_SECTION,
+    'short-description': Sections.SHORT_DESCRIPTION,
+    'yield': Sections.YIELDS_SECTION,
+    'yields': Sections.YIELDS_SECTION,
+    'yields-section': Sections.YIELDS_SECTION,
+}
+
+
+class Goldens(TestCase):
+
+    def setUp(self):
+        with open('integration_tests/goldens.json', 'r') as fin:
+            self.goldens = json.load(fin)
+
+    def parse_golden(self, golden):
+        if golden['type'] == 'GOOGLE':
+            docstring = Docstring.from_google(golden['docstring'])
+        elif golden['type'] == 'SPHINX':
+            docstring = Docstring.from_sphinx(golden['docstring'])
+        return docstring, golden['metadata']
+
+    def normalize_section(self, section):
+        lowered = section.lower().strip()
+        if lowered in SECTION_MAP:
+            return SECTION_MAP[lowered]
+        else:
+            raise Exception('Expected {} to be in section lookup.'.format(
+                section,
+            ))
+        return section
+
+    def assertSectionsMatch(self, docstring, metadata, message=''):
+        sections = metadata['sections']
+        for section in sections:
+            normalized_section = self.normalize_section(section)
+            self.assertTrue(
+                docstring.get_section(normalized_section),
+                'Expected docstring to have '
+                '{} but did not:\n\n```\n{}\n```\n\n{}'.format(
+                    normalized_section,
+                    docstring.root.reconstruct_string(),
+                    message,
+                )
+            )
+
+    def test_golden(self):
+        for i, golden in enumerate(self.goldens):
+            docstring, metadata = self.parse_golden(golden)
+            self.assertSectionsMatch(
+                docstring,
+                metadata,
+                'Index: {}'.format(i),
+            )
+            sys.stderr.write('.')
