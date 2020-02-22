a5071eae1d1ba79e888be3084c4ca5e782fc2623
Laurent Vermue <lvermue@users.noreply.github.com>
2020-02-22 01:23:22 +0100

    Add multi-line support for sphinx return sections

--- darglint/parse/grammars/sphinx_returns_section.py
+++ darglint/parse/grammars/sphinx_returns_section.py
@@ -15,9 +15,9 @@ from darglint.parse.identifiers import (
 
 class ReturnsGrammar(BaseGrammar):
     productions = [
-        P("returns-section", ([], "rethead", "returns-section1", 0)),
-        P("rethead", ([], "colon", "rethead0", 0)),
-        P("retbody", ([], "line", "block-indented", 0), ([], "word", "line", 0), ([], "word", "noqa-maybe", 0), ([NoqaIdentifier], "hash", "noqa", 0), ([NoqaIdentifier], "noqa-head", "noqa-statement1", 0), (TokenType.INDENT, 0), (TokenType.COLON, 0), (TokenType.HASH, 0), (TokenType.LPAREN, 0), (TokenType.RPAREN, 0), (TokenType.WORD, 0), (TokenType.RAISES, 0), (TokenType.ARGUMENTS, 0), (TokenType.ARGUMENT_TYPE, 0), (TokenType.RETURNS, 0), (TokenType.RETURN_TYPE, 0), (TokenType.YIELDS, 0), (TokenType.YIELD_TYPE, 0), (TokenType.VARIABLES, 0), (TokenType.VARIABLE_TYPE, 0), (TokenType.NOQA, 0)),
+        P("returns-section", ([], "rethead", "retbody", 0)),
+        P("rethead", ([], "colon", "rethead0", 0),  ([], "colon", "rethead2", 0)),
+        P("retbody", ([], "line", "newlines", 0), ([], "line", "retbody0", 0), ([], "word", "noqa-maybe", 0), ([NoqaIdentifier], "hash", "noqa", 0), ([NoqaIdentifier], "noqa-head", "noqa-statement1", 0), (TokenType.INDENT, 0), (TokenType.COLON, 0), (TokenType.HASH, 0), (TokenType.LPAREN, 0), (TokenType.RPAREN, 0), (TokenType.WORD, 0), (TokenType.RAISES, 0), (TokenType.ARGUMENTS, 0), (TokenType.ARGUMENT_TYPE, 0), (TokenType.RETURNS, 0), (TokenType.RETURN_TYPE, 0), (TokenType.YIELDS, 0), (TokenType.YIELD_TYPE, 0), (TokenType.VARIABLES, 0), (TokenType.VARIABLE_TYPE, 0), (TokenType.NOQA, 0)),
         P("block-indented", ([], "paragraph-indented", "block-indented0", 0), ([], "indented", "paragraph-indented0", 0), ([], "indented", "line", 0)),
         P("paragraph-indented", ([], "indented", "paragraph-indented0", 0), ([], "indented", "line", 0)),
         P("indented", ([], "indent", "indents", 0), (TokenType.INDENT, 0)),
@@ -35,8 +35,13 @@ class ReturnsGrammar(BaseGrammar):
         P("noqa-maybe", ([NoqaIdentifier], "hash", "noqa", 0), ([NoqaIdentifier], "noqa-head", "noqa-statement1", 0)),
         P("noqa-head", ([], "hash", "noqa", 0)),
         P("words", ([], "word", "words", 0), (TokenType.COLON, 0), (TokenType.HASH, 0), (TokenType.INDENT, 0), (TokenType.LPAREN, 0), (TokenType.RPAREN, 0), (TokenType.WORD, 0), (TokenType.RAISES, 0), (TokenType.ARGUMENTS, 0), (TokenType.ARGUMENT_TYPE, 0), (TokenType.RETURNS, 0), (TokenType.RETURN_TYPE, 0), (TokenType.YIELDS, 0), (TokenType.YIELD_TYPE, 0), (TokenType.VARIABLES, 0), (TokenType.VARIABLE_TYPE, 0), (TokenType.NOQA, 0)),
-        P("returns-section1", ([], "retbody", "newlines", 0), ([], "line", "block-indented", 0), ([], "word", "line", 0), ([], "word", "noqa-maybe", 0), ([NoqaIdentifier], "hash", "noqa", 0), ([NoqaIdentifier], "noqa-head", "noqa-statement1", 0), (TokenType.INDENT, 0), (TokenType.COLON, 0), (TokenType.HASH, 0), (TokenType.LPAREN, 0), (TokenType.RPAREN, 0), (TokenType.WORD, 0), (TokenType.RAISES, 0), (TokenType.ARGUMENTS, 0), (TokenType.ARGUMENT_TYPE, 0), (TokenType.RETURNS, 0), (TokenType.RETURN_TYPE, 0), (TokenType.YIELDS, 0), (TokenType.YIELD_TYPE, 0), (TokenType.VARIABLES, 0), (TokenType.VARIABLE_TYPE, 0), (TokenType.NOQA, 0)),
-        P("rethead0", ([], "returns", "colon", 0)),
+        P("rethead0", ([], "returns", "rethead1", 0)),
+        P("rethead1", ([], "colon", "newlines", 0), (TokenType.COLON, 0)),
+        P("rethead2", ([], "returns", "rethead3", 0)),
+        P("rethead3", ([], "newline", "rethead4", 0)),
+        P("rethead4", ([], "block-indented", "newlines", 0), ([], "paragraph-indented", "block-indented0", 0),([], "indented", "paragraph-indented0", 0), ([], "indented", "line", 0)),
+        P("retbody0", ([], "newline", "retbody1", 0)),
+        P("retbody1", ([], "block-indented", "newlines", 0), ([], "paragraph-indented", "block-indented0", 0), ([], "indented", "paragraph-indented0", 0), ([], "indented", "line", 0)),
         P("block-indented0", ([], "split", "block-indented", 0)),
         P("paragraph-indented0", ([], "line", "paragraph-indented1", 0)),
         P("paragraph-indented1", ([], "newline", "paragraph-indented", 0)),
